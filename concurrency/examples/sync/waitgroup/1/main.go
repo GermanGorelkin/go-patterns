22ebe85780b81ac1f6f14cf97a7aca539bdc7ed0
German Gorelkin <german.gorelkin@gmail.com>
2020-02-08 22:08:14 +0300

    add example of sync.WaitGroup

---
+++ concurrency/examples/sync/waitgroup/1/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println("1st goroutine sleeping...")
+		time.Sleep(1)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println("2nd goroutine sleeping...")
+		time.Sleep(2)
+	}()
+
+	wg.Wait()
+	fmt.Println("All goroutines complete.")
+}
